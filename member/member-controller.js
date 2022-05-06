b6bca9ab38b4d7658e044d7f19ca20a3f473d733
kim-dongwan <104421644+kim-dongwan@users.noreply.github.com>
2022-05-06 15:10:17 +0900

    Add files via upload

--- member/member-controller.js
+++ member/member-controller.js
@@ -4,15 +4,15 @@ module.exports.Loginpost = async function(req, res){
 	 var result =  await memberModel.Login(req);  
 	 
 	 var errMessage = "";
-	if (result.output.ReturnVal==400)
+	if (result.output.ReturnVal===400)
 	{
 		errMessage ="ID,PW inconsistency!"; //계정정보틀림
 	}
-	else if (result.output.ReturnVal==401)
+	else if (result.output.ReturnVal===401)
 	{
 		errMessage ="inactive member"; //휴면계정
 	}
-	else if (result.output.ReturnVal==402)
+	else if (result.output.ReturnVal===402)
 	{
 		errMessage ="secession member"; //탈퇴계정
 	}
